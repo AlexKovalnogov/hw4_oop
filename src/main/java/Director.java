55bdf30be21205647ad840a7421f1e725b9df23a
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2019-01-21 14:12:22 +0200

    add functions into class director

--- src/main/java/Director.java
+++ src/main/java/Director.java
@@ -4,6 +4,39 @@ public class Director extends Employee{
 
     private Logger logger= Logger.getLogger(Director.class);
 
+    public void changeLocationOfServiceCenter() {
+        System.out.print("changeLocationOfServiceCenter");
+    }
+
+    public void hireEmployee() {
+        System.out.println("hireEmployee");
+    }
+
+    public void fireEmployee() {
+        System.out.println("fireEmployee");
+    }
+
+    public void payTax() {
+        System.out.print("payTax");
+    }
+
+    public void paySalary() {
+        System.out.print("paySalary");
+    }
+
+
+    public void getListOfEmployees() {
+        System.out.println("getListOfEmployees");
+    }
+
+    public void increaseSalaryForEmployees() {
+        System.out.println("increaseSalaryForEmployees");
+    }
+
+    public void getRevenues() {
+        System.out.println("getRevenues");
+    }
+
 
 
 }
