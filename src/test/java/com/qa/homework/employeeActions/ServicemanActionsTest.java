dc3fb086f7dadf2b557c00cf084b4591368a7d9c
Home <kovalnogovalex@gmail.com>
2019-02-05 02:45:24 +0300

    tests1

---
+++ src/test/java/com/qa/homework/employeeActions/ServicemanActionsTest.java
@@ -0,0 +1,18 @@
+package com.qa.homework.employeeActions;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ServicemanActionsTest {
+
+
+
+    @Test
+    public void testFixProblemWithEquipment() {
+    }
+
+    @Test
+    public void testGiveEquipment() {
+    }
+}
